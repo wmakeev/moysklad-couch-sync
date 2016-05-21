e7d779384fac85d08cb2a3620dc15ffad4ac2651
Makeev Vitaliy <w.makeev@gmail.com>
2016-05-21 16:03:07 +0500

    typings #1

---
+++ project.d.ts
@@ -0,0 +1,63 @@
+interface CouchDBView<T> {
+    key: string;
+    id?: string;
+    value?: T
+    error?: string
+}
+
+interface CouchDBList<T> {
+    offset: number;
+    rows: Array<CouchDBView<T>>;
+    total_rows: number;
+}
+
+/**
+ * Запись в базе данных CouchDB
+ */
+interface CouchDBDoc {
+    /** Идентификатор токена в базе базе данных CouchDB */
+    _id: string;
+    /** Ревизия токена в базе базе данных CouchDB */
+    _rev: string;
+}
+
+/**
+ * Токен указывающий на точку где была приостановлена очередная итерация синхронизация
+ */
+interface ContinuationToken extends CouchDBDoc {
+    /** Тип объекта */
+    TYPE_NAME: string;
+    /** Дата в формате UTC с которой должна быть начата следующая итерация синхронизации */
+    updatedFrom: string;
+    /** Дата в формате UTC до которой должна быть завершена синхронизация */
+    updatedTo: string;
+    /** Идентификатор на котором была приостановлена прошлая итерация синхронизации */
+    fromUuid?: string;
+}
+
+/**
+ * Сущность МойСклад
+ */
+interface Entity {
+    TYPE_NAME: string;
+    uuid: string;
+    name: string;
+    updated: Date;
+}
+
+interface EntityCollection<T> extends ArrayLike<T> {
+    total: number
+    start: number
+    count: number
+}
+
+interface Query {}
+
+interface CouchEntity extends Entity, CouchDBDoc {}
+
+declare module "_project/nano-promise" {
+
+    /** Возвращает список ревизий для объектов с указанными ключами */
+    export function fetchRevs(query: { keys: Array<string> }): Promise<CouchDBList<{ rev: string }>>
+
+}
