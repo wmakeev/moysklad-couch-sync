73280527db2eb28902ea4b6aedb20a34bf958ac4
Makeev Vitaliy <w.makeev@gmail.com>
2016-05-21 16:01:01 +0500

    Рефакторинг #1
    Удаление, переименование, перемещение файлов

--- src/update-continuation-token.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = continuationToken =>
-  updated => updated instanceof Date
-    ? Object.assign({}, continuationToken, { updated })
-    : Object.assign({}, continuationToken, updated)
